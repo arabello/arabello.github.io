ce9abb642e691325d32390f86690da67e9f51b96
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2020-09-12 19:02:28 +0200

    Add mailgo plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,14 @@
 $(function(){
-    obfuscateEmail();
-    $(window).bind('hashchange', onURLChange);
+    let mailgoConfig = {
+        actions: {
+            yahoo: false,
+            gmail: false,
+            outlook: false,
+        }
+    };
+
+    mailgo(mailgoConfig);
 });
 
-function obfuscateEmail() {
-  var obfuscatedEmail = span = '';
 
-  email = $('.email').first().html();
 
-  for (var i = 0; i < email.length; ++i) {
-    span = '<span>' + email[i] + '</span>';
-    obfuscatedEmail += span;
-  }
-
-  $('.email').each(function(){
-      $(this).html(obfuscatedEmail);
-  });
-}
-
-function onURLChange(){
-    $(".side-link").each(function(index){
-        $(this).removeClass("primary-"+index);
-        $(".masthead-title").removeClass("primary-"+index);
-        if ($(this).attr("href") == window.location.hash){
-            $(this).addClass("primary-"+index);
-            $(".masthead-title").addClass("primary-"+index);
-        }
-    });
-}
