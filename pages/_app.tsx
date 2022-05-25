b2c913abc43a9180d9b9190822ceb5a0e53992d5
matteo-pellegrino <matteo.pellegrino@buildo.io>
2022-05-25 09:10:25 +0200

    Add bootstrap-icons as dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/bootstrap-override.css";
 import type { AppProps } from "next/app";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ const Document = () => (
   <Html>
     <Head>
       <link rel="icon" href="/favicon.svg" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css"
-      />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <body className="min-vh-100">
