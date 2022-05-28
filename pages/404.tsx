4f101664b021946a78aba457b83c1c992e851a7c
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2022-05-28 19:08:32 +0200

    refactor: update metadescriptions

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,16 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const Custom404 = () => (
   <div className="container-fluid d-flex flex-column min-vh-100">
+    <Head>
+      <title>Matteo Pellegrino | 404 Not Found</title>
+      <meta
+        name="description"
+        content="404 page not found on matteopellegrino.me"
+      ></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
     <div className="row min-vh-100 justify-content-center align-items-center">
       <div>
         <h1 className="display-4 text-center">404</h1>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ const Document = () => (
   <Html>
     <Head>
       <link rel="icon" href="/favicon.svg" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <body className="min-vh-100">
       <Main />
