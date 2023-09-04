9c5d69c27ea25d2bea0c2bab8dd84cd7f6b6aea9
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2023-09-04 23:09:14 +0200

    feat: Migrate contacts card

--- pages/components/ContactsCard.tsx
+++ pages/components/ContactsCard.tsx
@@ -1,52 +1,50 @@
+import {
+  ButtonLink,
+  IconDocument,
+  IconMail,
+  IconSocialLinkedIn,
+  Stack,
+  Text,
+} from "braid-design-system";
 import Card from "./Card";
-import Image from "next/image";
 
 const ContactsCard = () => (
   <Card>
-    <div className="text-center">Let&apos;s keep in touch 👇</div>
-    <a
-      type="button"
-      className="btn btn-light border-0 align-items-center d-flex gap-2 justify-content-center mt-2"
-      href="/assets/matteo-pellegrino-cv.pdf"
-      target="blank"
-    >
-      <Image
-        src="/assets/icons/file-text.svg"
-        width={24}
-        height={24}
-        alt="curriculum vitae icon"
-      />
-      <span>Curriculum Vitae</span>
-    </a>
-    <a
-      type="button"
-      className="btn btn-light border-0 align-items-center d-flex gap-2 justify-content-center mt-2"
-      href="#mailgo"
-      data-address="matteo.pelle.pellegrino"
-      data-domain="gmail.com"
-    >
-      <Image
-        src="/assets/icons/mail.svg"
-        width={24}
-        height={24}
-        alt="email icon"
-      />
-      <span>Drop me a line</span>
-    </a>
-    <a
-      type="button"
-      className="btn btn-light border-0 align-items-center d-flex gap-2 justify-content-center mt-2"
-      href="https://www.linkedin.com/in/mttpll"
-      target="blank"
-    >
-      <Image
-        src="/assets/icons/linkedin.svg"
-        width={24}
-        height={24}
-        alt="linkedin icon"
-      />
-      <span>Linkedin</span>
-    </a>
+    <Stack space="xsmall" align="center">
+      <Text>Let&apos;s keep in touch 👇</Text>
+      <Text />
+      <ButtonLink
+        href="/assets/matteo-pellegrino-cv.pdf"
+        target="blank"
+        variant="soft"
+        size="small"
+        icon={<IconDocument />}
+        tone="neutral"
+      >
+        Curriculum Vitae
+      </ButtonLink>
+      <ButtonLink
+        data-address="matteo.pelle.pellegrino"
+        data-domain="gmail.com"
+        href="#mailgo"
+        variant="soft"
+        size="small"
+        icon={<IconMail />}
+        tone="neutral"
+      >
+        Drop me a line
+      </ButtonLink>
+      <ButtonLink
+        href="https://www.linkedin.com/in/mttpll"
+        target="blank"
+        variant="soft"
+        size="small"
+        icon={<IconSocialLinkedIn />}
+        tone="neutral"
+      >
+        LinkedIn
+      </ButtonLink>
+    </Stack>
   </Card>
 );
 
