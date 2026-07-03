8497f508868a57a537a895d58aa04691bc0400f4
Matteo Pellegrino <matteo@northsea.co>
2026-07-03 19:02:36 +0200

    fix: Prerender /music

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   async prerender() {
-    return ["/", "/books", "/quotes", "/posts/ts-gadt", "/ai-news"];
+    return ["/", "/books", "/music", "/quotes", "/posts/ts-gadt", "/ai-news"];
   },
 } satisfies Config;
